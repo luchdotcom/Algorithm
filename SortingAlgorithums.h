ae2df7ed798efa69f72d6f64877874eb870d6dbf
Luchdotcom <lachezarivanov05@gmail.com>
2022-05-26 22:58:35 +0300

    add mergeSort algorithm

---
+++ SortingAlgorithums.h
@@ -0,0 +1,9 @@
+//
+// Created by luchdotcom on 24.05.22.
+//
+
+#ifndef DEMO_SORTINGALGORITHUMS_H
+#define DEMO_SORTINGALGORITHUMS_H
+void mergeSort(int* array, const int begin,const int end);
+void merge(int* array, int const left, int const mid, int const right);
+#endif //DEMO_SORTINGALGORITHUMS_H
